01039a5d827f3901c0fd7c756bfd2be077281aed
Jono Mingard <reason.koan@gmail.com>
2015-09-21 21:35:45 +1200

    Add Drawable interface for players and items

--- src/client/renderer/Drawable.java
+++ src/client/renderer/Drawable.java
@@ -1,4 +1,23 @@
 package client.renderer;
 
 public interface Drawable {
+    class BoundingCube {
+        public final int x;
+        public final int width;
+        public final int y;
+        public final int height;
+        public final int z;
+        public final int depth;
+
+        public BoundingCube(int x, int y, int z, int width, int height, int depth) {
+            this.x = x;
+            this.width = width;
+            this.y = y;
+            this.height = height;
+            this.z = z;
+            this.depth = depth;
+        }
+    }
+
+    BoundingCube getBoundingCube();
 }
