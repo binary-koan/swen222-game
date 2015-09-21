624d08335e7ae8faf41050cc1c1a9bc8fdcf54fd
Jono Mingard <reason.koan@gmail.com>
2015-09-21 22:07:17 +1200

    Give players the ability to emit movement events

--- src/game/Player.java
+++ src/game/Player.java
@@ -1,6 +1,9 @@
 package game;
 
 import client.renderer.Drawable;
+import client.renderer.RoomRenderer;
+
+import java.awt.*;
 
 public class Player implements Drawable {
     private Room room;
@@ -12,6 +15,19 @@ public class Player implements Drawable {
 
     public Position getPosition() {
         return position;
+    }
+
+    public void addMovementListener(MovementListener movementListener) {
+    }
+
+    @Override
+    public BoundingCube getBoundingCube() {
+        return null;
+    }
+
+    @Override
+    public Image getSprite(Position position) {
+        return null;
     }
 
     public enum Position {
