b5599137bc6258d0ec36fbc72917f5d227e81c91
Shanzii <shanon.beary@gmail.com>
2015-09-29 12:45:54 +1300

    Random Idea

--- src/gameworld/Door.java
+++ src/gameworld/Door.java
@@ -1,5 +1,14 @@
 package gameworld;
 
+/*
+ * Not sure if will use this class, was just thinking
+ * could be used to connect rooms or for some checking
+ * of some sorts
+ * 
+ */
 public class Door {
 
+	public Door() {
+		
+	}
 }
