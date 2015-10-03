316de447f7015758f4bada16aa441b4bb44442a7
Shanzii <shanon.beary@gmail.com>
2015-10-04 12:43:51 +1300

    Now can add items to chest

--- src/game/Chest.java
+++ src/game/Chest.java
@@ -1,16 +1,32 @@
 package game;
 
+import java.util.*;
+
 public class Chest extends Item{
 
+	private List<Item> chestItems;
 	
-	public Chest(String name, String spriteName) {
+	public Chest(String name, String spriteName, Item...items) {	
 		super(name, spriteName, false);
+		this.chestItems = new ArrayList<Item>();
+		int j = 0;
+		for(Item i : items) {
+			if (j <= 3) { 
+				this.chestItems.add(i);
+			}
+			j++;
+		}
+		
 	}
 
 	public String quiz() {
 		return "Whats 2 + 5?";
 	}
 
+	public List<Item> getItems() {
+		return chestItems;
+	}
+	
 	@Override
 	public Direction getFacingDirection() {
 		// TODO Auto-generated method stub
