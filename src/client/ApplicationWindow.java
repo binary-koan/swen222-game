8b3726a9b531ade6abc41edcd200c842889a2abd
Jono Mingard <reason.koan@gmail.com>
2015-10-04 13:42:07 +1300

    Fix BoundingCube references in ApplicationWindow

--- src/client/ApplicationWindow.java
+++ src/client/ApplicationWindow.java
@@ -9,7 +9,6 @@ import game.Chest;
 import game.Key;
 import game.Player;
 import game.Room;
-import game.Drawable.BoundingCube;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
@@ -32,40 +31,33 @@ import client.renderer.ResourceLoader;
 public class ApplicationWindow extends JFrame {
 
 	private GameCanvas canvas;
-	
+
 	public ApplicationWindow(String title, GameCanvas canvas) {
 		super(title);
 		this.canvas = canvas;
-		
+
 		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
 		Dimension scale = new Dimension();
 		scale.setSize(screenSize.getWidth() * 0.6, screenSize.getHeight() * 0.7);
 		setPreferredSize(scale);
-		
-		
-		
+
 		final JMenuBar menuBar = setupMenuBar();
 		JPanel lowerBar = setupLowerBar();
 		setLayout(new BorderLayout());
 
-		
 		//Add items to the frame
 		add(menuBar, BorderLayout.NORTH);
 		add(canvas, BorderLayout.CENTER);
 		add(lowerBar, BorderLayout.SOUTH);
-		
-		
-		
+
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 	}
 
-	
-	
-
 	public GameCanvas getGameCanvas() {
 		return canvas;
 	}
+
 	private JMenuBar setupMenuBar() {
 		JMenuBar menuBar = new JMenuBar();
 
@@ -101,38 +93,38 @@ public class ApplicationWindow extends JFrame {
 	private JPanel setupLowerBar() {
 		JPanel area = new JPanel();
 		area.setLayout(new BorderLayout());
-		
+
 		JPanel inventory = new JPanel();
 		inventory.setLayout(new FlowLayout());
 		inventory.add(new ImagePanel("key"));
 		inventory.add(new ImagePanel("fireplace"));
-		
+
 		//inventory.setBackground(Color.BLACK);
-		
+
 		area.add(inventory, BorderLayout.EAST);
 		return area;
 	}
-	
+
 	private class ImagePanel extends JPanel {
 		private BufferedImage image;
-		
+
 		public ImagePanel(String item) {
 			try {
 				image = ImageIO.read(new File("resources/objects/"+item+".png"));
 				image = image.getSubimage(0, 0, image.getWidth(), image.getHeight());
 			} catch (IOException ex) {
-				
+
 			}
-			
+
 			this.setPreferredSize(new Dimension(40, 40));
 			this.setBackground(Color.DARK_GRAY);
-		
+
 		}
-		
+
 		protected void paintComponent(Graphics g) {
 			super.paintComponent(g);
 			g.drawImage(image, 5, 5, this.getWidth() - 5, this.getHeight() - 5, null);
-		
+
 		}
 	}
 
@@ -149,29 +141,23 @@ public class ApplicationWindow extends JFrame {
 
             {
             	 Item item = new Bed("Bed", "objects/bed.png");
-                 getItems().add(new Room.ItemInstance(item, Direction.NORTH, new Drawable.BoundingCube(80, 0, 80, 48, 32, 48)));
+                 getItems().add(new Room.ItemInstance(item, Direction.NORTH, new Drawable.Point3D(80, 0, 80)));
 
                  item = new Chest("Chest", "objects/chest.png");
-                 getItems().add(new Room.ItemInstance(item, Direction.EAST, new Drawable.BoundingCube(40, 0, 120, 48, 32, 48)));
+                 getItems().add(new Room.ItemInstance(item, Direction.EAST, new Drawable.Point3D(40, 0, 120)));
 
                  item = new Key("Key", "objects/key.png");
-                 getItems().add(new Room.ItemInstance(item, Direction.NORTH, new Drawable.BoundingCube(140, 60, 20, 32, 32, 32)));
+                 getItems().add(new Room.ItemInstance(item, Direction.NORTH, new Drawable.Point3D(140, 60, 20)));
 
                  item = new Door("Door", "objects/door.png");
-                 getItems().add(new Room.ItemInstance(item, Direction.WEST, new Drawable.BoundingCube(140, 0, 80, 32, 48, 32)));
+                 getItems().add(new Room.ItemInstance(item, Direction.WEST, new Drawable.Point3D(140, 0, 80)));
 
-                 player2.setBoundingBox(new Drawable.BoundingCube(80, 80, 80, 32, 32, 32));
                  getPlayers().add(player2);
             }
         };
         player2.setRoom(room);
 
         final Player player = new Player("Player 1", "characters/o1.png") {
-            @Override
-            public BoundingCube getBoundingCube() {
-                return new BoundingCube(80, 0, 80, 20, 20, 20);
-            }
-
             @Override
             public Room getRoom() {
                 return room;
@@ -201,21 +187,21 @@ public class ApplicationWindow extends JFrame {
             @Override
             public void keyReleased(KeyEvent e) {
             }
-       
+
         };
-        
+
         canvas.addKeyListener(keyListener);
 
 		SwingUtilities.invokeLater(new Runnable() {
             public void run() {
-            	
+
                 ApplicationWindow aw = new ApplicationWindow("Game", canvas);
                 aw.addKeyListener(keyListener);
                 aw.pack();
                 aw.setVisible(true);
-               
+
                 aw.getGameCanvas().setPlayer(player);
-                
+
             }
         });
 	}
