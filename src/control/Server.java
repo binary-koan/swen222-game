9bed536b0d989373fe2ad156a8f1141163a5a81d
BuildTools <unconfigured@null.spigotmc.org>
2015-10-04 14:59:10 +1300

    Updated server to latest

--- src/control/Server.java
+++ src/control/Server.java
@@ -1,25 +1,6 @@
 package control;
 
-// This file is part of the Multi-player Pacman Game.
-//
-// Pacman is free software; you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published
-// by the Free Software Foundation; either version 3 of the License,
-// or (at your option) any later version.
-//
-// Pacman is distributed in the hope that it will be useful, but
-// WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See
-// the GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public
-// License along with Pacman. If not, see <http://www.gnu.org/licenses/>
-//
-// Copyright 2010, David James Pearce.
-
-
 import game.Game;
-
 import java.util.*;
 import java.io.*;
 import java.net.*;
@@ -32,11 +13,13 @@ import java.net.*;
  */
 public final class Server extends Thread {
 
+	private final Game game;
 	private final int broadcastClock;
 	private final int uid;
 	private final Socket socket;
 
 	public Server(Socket socket, int uid, int broadcastClock, Game game) {
+		this.game = game;
 		this.broadcastClock = broadcastClock;
 		this.socket = socket;
 		this.uid = uid;
@@ -45,20 +28,21 @@ public final class Server extends Thread {
 	public void run() {
 		try {
 			DataInputStream input = new DataInputStream(socket.getInputStream());
-			DataOutputStream output = new DataOutputStream(socket.getOutputStream());
+			DataOutputStream output = new DataOutputStream(
+					socket.getOutputStream());
 			// First, write the period to the stream
-			boolean exit=false;
-			while(!exit) {
+			boolean exit = false;
+			while (!exit) {
 				try {
-					if(input.available() != 0) {
+					if (input.available() != 0) {
 
 						Thread.sleep(broadcastClock);
 					}
-				} catch(InterruptedException e) {
+				} catch (InterruptedException e) {
 				}
 			}
 			socket.close(); // release socket ... v.important!
-		} catch(IOException e) {
+		} catch (IOException e) {
 			System.err.println("PLAYER " + uid + " DISCONNECTED");
 
 		}
