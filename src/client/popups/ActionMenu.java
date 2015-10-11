b08a5bfb8e142262907a4a8b4ccdeb25e16695e9
Jono Mingard <reason.koan@gmail.com>
2015-10-11 16:17:42 +1300

    Improve popup documentation

--- src/client/popups/ActionMenu.java
+++ src/client/popups/ActionMenu.java
@@ -1,18 +1,27 @@
 package client.popups;
 
-import client.GameCanvas;
 import game.ActionReceiver;
 import game.Item;
+import org.eclipse.jdt.annotation.NonNull;
 
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+/**
+ * A popup menu showing all the actions which can be taken on a particular item
+ */
 public class ActionMenu extends JPopupMenu implements ActionListener {
-    private Item item;
-    private ActionReceiver receiver;
+    private @NonNull Item item;
+    private @NonNull ActionReceiver receiver;
 
-    public ActionMenu(ActionReceiver receiver, Item item) {
+    /**
+     * Create a new action menu
+     *
+     * @param receiver object which will be notified when an action is selected
+     * @param item item to display actions of
+     */
+    public ActionMenu(@NonNull ActionReceiver receiver, @NonNull Item item) {
         this.receiver = receiver;
         this.item = item;
 
@@ -21,8 +30,10 @@ public class ActionMenu extends JPopupMenu implements ActionListener {
         }
     }
 
+    /** {@inheritDoc} */
     @Override
     public void actionPerformed(ActionEvent e) {
+        // Figure out which action was triggered and send it off to the receiver
         for (Item.Action action : item.getAllowedActions()) {
             if (e.getActionCommand().equals(action.toString())) {
                 receiver.performAction(item, action);
@@ -31,6 +42,9 @@ public class ActionMenu extends JPopupMenu implements ActionListener {
         }
     }
 
+    /**
+     * Add an item to this menu
+     */
     private void addMenuItem(String text) {
         JMenuItem menuItem = new JMenuItem(text);
         menuItem.setName("actionMenuItem");
--- src/client/popups/InfoTooltip.java
+++ src/client/popups/InfoTooltip.java
@@ -2,11 +2,16 @@ package client.popups;
 
 import client.ResourceLoader;
 import game.Item;
+import org.eclipse.jdt.annotation.NonNull;
+import org.eclipse.jdt.annotation.Nullable;
 
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 import java.awt.*;
 
+/**
+ * A tooltip showing information about an item or player
+ */
 public class InfoTooltip extends JPanel {
     private Item.Action primaryAction;
     private Item.Action secondaryAction;
@@ -16,7 +21,12 @@ public class InfoTooltip extends JPanel {
 	private JLabel primaryActionLabel = new JLabel();
 	private JLabel secondaryActionLabel = new JLabel();
 
-	public InfoTooltip(ResourceLoader loader) {
+    /**
+     * Create a blank info tooltip
+     *
+     * @param loader used to load mouse click icons
+     */
+	public InfoTooltip(@NonNull ResourceLoader loader) {
 		setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
 		setBorder(new EmptyBorder(10, 10, 10, 10));
 
@@ -41,14 +51,24 @@ public class InfoTooltip extends JPanel {
 		add(secondaryActionLabel);
 	}
 
-	public void showObject(String name, Item.Action primary, Item.Action secondary) {
+    /**
+     * Update the tooltip labels to show information about the
+     *
+     * @param name the name of the object
+     * @param primary action to be taken when left-clicking the object
+     * @param secondary action to be taken when right-clicking the object
+     */
+	public void showObject(@NonNull String name, Item.Action primary, Item.Action secondary) {
         primaryAction = primary;
         secondaryAction = secondary;
 
         descriptionLabel.setVisible(false);
 		nameLabel.setText(name.toUpperCase());
 
-        if (primary != null) {
+        if (primary == null) {
+            primaryActionLabel.setVisible(false);
+        }
+        else {
             primaryActionLabel.setText(primary.toString());
         }
 
@@ -61,17 +81,32 @@ public class InfoTooltip extends JPanel {
 		}
 	}
 
+    /**
+     * Display a description of the current item below its name
+     *
+     * @param description text to display
+     */
+    public void showDescription(@NonNull String description) {
+        descriptionLabel.setVisible(true);
+        // Using HTML in the label makes it word wrap and allows for markup in descriptions
+        descriptionLabel.setText("<html>" + description + "</html>");
+    }
+
+    /**
+     * Returns the action currently being displayed as the primary (left-click) action
+     *
+     * @return the action being displayed
+     */
     public Item.Action getPrimaryAction() {
         return primaryAction;
     }
 
+    /**
+     * Returns the action currently being displayed as the secondary (right-click) action
+     *
+     * @return the action being displayed
+     */
     public Item.Action getSecondaryAction() {
         return secondaryAction;
-    }
-
-    public void showDescription(String description) {
-        descriptionLabel.setVisible(true);
-        // Using HTML in the label makes it word wrap and allows for markup in descriptions
-        descriptionLabel.setText("<html>" + description + "</html>");
     }
 }
