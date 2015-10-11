01d25a2d147102340cd647b50fff6d77c89116ac
Jono Mingard <reason.koan@gmail.com>
2015-10-11 17:57:37 +1300

    Clean up Player class and add documentation

--- src/game/Player.java
+++ src/game/Player.java
@@ -7,6 +7,8 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 
+import org.eclipse.jdt.annotation.NonNull;
+import org.eclipse.jdt.annotation.Nullable;
 import org.jdom2.Document;
 import org.jdom2.Element;
 import org.jdom2.JDOMException;
@@ -14,7 +16,13 @@ import org.jdom2.input.SAXBuilder;
 import org.jdom2.output.Format;
 import org.jdom2.output.XMLOutputter;
 
-public class Player implements Drawable, Serializable{
+/**
+ * Represents a player's avatar in the game
+ */
+public class Player implements Drawable, Serializable {
+    /**
+     * Exception thrown if the player is given an invalid movement command
+     */
 	public class InvalidMovementException extends Exception { }
 
 	private String name;
@@ -23,29 +31,85 @@ public class Player implements Drawable, Serializable{
     private Direction facingDirection;
     private Item heldItem;
 
+    /**
+     * Create a new player
+     *
+     * @param name player name
+     * @param spriteName sprite used to render the player
+     * @param room room the player is initially in
+     */
     public Player(String name, String spriteName, Room room) {
     	this.name = name;
     	this.spriteName = spriteName;
         this.room = room;
+        this.facingDirection = Direction.NORTH;
+
         room.addPlayer(this);
     }
 
-    public String getName(){
+    // Getters
+
+    /**
+     * @return the name of the player
+     */
+    public String getName() {
     	return name;
     }
 
+    /**
+     * @return the room that the player is in
+     */
     public Room getRoom() {
         return room;
     }
 
+    /**
+     * @return the direction the player is facing
+     */
     public Direction getFacingDirection() {
         return facingDirection;
     }
 
+    /**
+     * @return a point near the center of the wall the player is viewing their room from
+     */
+    @Override
+    public Point3D getPosition() {
+        switch (this.facingDirection) {
+            case NORTH:
+                return new Point3D(160, 0, 10);
+            case SOUTH:
+                return new Point3D(160, 0, 310);
+            case EAST:
+                return new Point3D(310, 0, 160);
+            case WEST:
+            default:
+                return new Point3D(10, 0, 160);
+        }
+    }
+
+    @Override
+    public String getSpriteName() {
+        return spriteName;
+    }
+
+    // Actions
+
+    /**
+     * Set the player's facing direction
+     *
+     * @param facingDirection new direction the player should face
+     */
     public void turn(Direction facingDirection) {
         this.facingDirection = facingDirection;
     }
 
+    /**
+     * Move to the adjacent room in the given direction
+     *
+     * @param movementDirection direction to move in
+     * @throws InvalidMovementException if there is no path in the given direction
+     */
 	public void move(Direction movementDirection) throws InvalidMovementException {
 		Room newRoom = room.getConnection(movementDirection);
 
@@ -59,25 +123,7 @@ public class Player implements Drawable, Serializable{
 		}
 	}
 
-    @Override
-    public Point3D getPosition() {
-    	switch (this.facingDirection) {
-    	case NORTH:
-    		return new Point3D(160, 0, 10);
-    	case SOUTH:
-    		return new Point3D(160, 0, 310);
-    	case EAST:
-    		return new Point3D(310, 0, 160);
-    	case WEST:
-		default:
-    		return new Point3D(10, 0, 160);
-    	}
-    }
-
-    @Override
-    public String getSpriteName() {
-        return spriteName;
-    }
+    // Serialization
 
 	@Override
 	public Element toXML() {
